fba7d9c8cbc4edfa115c205ec68c3273c1ef9ceb
GROMIT <przem9.pp@gmail.com>
2025-06-26 17:32:37 +0200

    added base class for entities

---
+++ src/entities/entity.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "raylib.h"
+#include <limits.h>
+#include <assert.h>
+
+class Entity
+{
+public:
+	Vector2 pos;
+
+	Entity(float x, float y);
+	Entity(const Vector2& pos);
+	Entity(const Entity& entity);
+	inline long long getId() { return id; }
+	virtual void Update() = 0;
+private:
+	long long id;
+	static long long idCounter;
+
+};
+
+Entity::Entity(float x, float y)
+	:pos({x,y})
+{
+	static long long idCounter = 0;
+
+	if (idCounter == LLONG_MAX)
+	{
+		//LOG run out of id's
+		assert(false && "no more id's");
+	}
+
+	id = idCounter;
+	idCounter++;
+}
+
+Entity::Entity(const Vector2& pos)
+	:Entity(pos.x, pos.y)
+{}
+
+Entity::Entity(const Entity& entity)
+	:Entity(entity.pos)
+{}

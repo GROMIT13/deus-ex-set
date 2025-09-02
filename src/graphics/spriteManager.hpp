7606183ad74d8a40af5baffe41129d694179a26c
GROMIT13 <przem9.pp@gmail.com>
2025-09-02 21:25:22 +0200

    added sprite manager

--- src/graphics/spriteManager.hpp
+++ src/graphics/spriteManager.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include "raylib.h"
+#include "sprite.hpp"
+#include <unordered_map>
 
 enum class SpriteName
 {
@@ -14,20 +17,55 @@ class SpriteManager
 public:
 	SpriteManager();
 	void LoadSprite(SpriteName spriteName);
+	void UnloadSprite(SpriteName spriteName);
 	~SpriteManager();
 
 private:
-	static char textureNames[] = { "test16.png",test32.png,test64.png,test128.png,test256.png };
+	std::unordered_map<SpriteName, Sprite> sprites;
+	std::unordered_map<SpriteName, const char*> spriteNames = {
+		   {SpriteName::TEST16, "test16.png"},
+		   {SpriteName::TEST32, "test32.png"},
+		   {SpriteName::TEST64, "test64.png"},
+		   {SpriteName::TEST128, "test128.png"},
+		   {SpriteName::TEST256, "test256.png"}
+	};
 };
 
 SpriteManager::SpriteManager()
 {
+
 }
 
 void SpriteManager::LoadSprite(SpriteName spriteName)
 {
+	try
+	{	
+		auto hasBeenLoaded = sprites.try_emplace(spriteName, spriteNames.at(spriteName));
+
+		if (!hasBeenLoaded.second)
+			Log::Warn(TextFormat("sprite with texture %s is already loaded", spriteNames.at(spriteName)));
+	}
+	catch (const std::out_of_range)
+	{
+		Log::Error(TextFormat("file for SpriteName with number %i doesn't exist, initialize spriteName", spriteName));
+	}
+}
+
+void SpriteManager::UnloadSprite(SpriteName spriteName)
+{
+	try
+	{
+		size_t erasedCount = sprites.erase(spriteName);
+		if (erasedCount == 0)
+			Log::Warn(TextFormat("sprite with texture %s wasn't loaded so it can't be unloaded", spriteNames.at(spriteName)));
+	}
+	catch (const std::out_of_range)
+	{
+		Log::Error(TextFormat("file for SpriteName with number %i does not exist, initialize spriteName", spriteName));
+	}
 }
 
 SpriteManager::~SpriteManager()
 {
+
 }

34f5f2f0c9096c6e53e39a3593e0452cb549b9a3
GROMIT13 <przem9.pp@gmail.com>
2025-09-04 01:42:24 +0200

    removed function

--- src/graphics/draw.hpp
+++ src/graphics/draw.hpp
@@ -9,8 +9,3 @@ void DrawCircleV(Vector2 center, float radius, Color color, Scene* scene)
 	Vector2 drawPos = scene->GetScreenSpace().ToThisSpace(center, scene->GetGameSpace());
 	DrawCircleV(drawPos, radius * Window::Scale(), color);
 }
-
-void DrawTexturePro()
-{
-
-}

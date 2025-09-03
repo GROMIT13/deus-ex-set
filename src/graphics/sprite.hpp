675e686f5230ea2f0efe51440f9e316130104826
GROMIT13 <przem9.pp@gmail.com>
2025-09-04 01:43:00 +0200

    added sprite class

--- src/graphics/sprite.hpp
+++ src/graphics/sprite.hpp
@@ -2,11 +2,15 @@
 
 #include "raylib.h"
 
+class Scene;
+
 class Sprite
 {
 public:
-	Sprite(const Texture2D& texture2D, const Vector2& dimensions);
-	void Draw();
+	Sprite(const char* file);
+	Sprite(const char* file, TextureFilter filter, TextureWrap wrap);
+	void Draw(const Vector2& pos, float rotation, Color color);
+	void Draw(const Vector2& pos, float rotation, Color color, Scene* scene);
 	~Sprite();
 
 private:
@@ -14,20 +18,3 @@ private:
 	Rectangle sourceRect;
 	Vector2 dimensions; //Dimensions specified in game units
 };
-
-Sprite::Sprite(const Texture2D& texture2D,const Vector2& dimensions)
-	:texture2D(texture2D),
-	 sourceRect({ 0.0f, 0.0f, (float)texture2D.height, (float)texture2D.width }),
-	 dimensions(dimensions)
-{
-}
-
-void Sprite::Draw()
-{
-	//Rectangle = { ,,, };
-	DrawTexturePro(texture2D, sourceRect, destRect16, { destRect16.width / 2.0f,destRect16.height / 2.0f }, 0, WHITE);
-}
-
-Sprite::~Sprite()
-{
-}

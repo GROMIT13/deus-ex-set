e4db3ac32977fd76e20bb0d66765bd6f6b10d6db
GROMIT13 <przem9.pp@gmail.com>
2025-09-08 21:51:05 +0200

    added level

--- src/scenes/baseLevel.cpp
+++ src/scenes/baseLevel.cpp
@@ -32,26 +32,7 @@ LevelScene::LevelScene(const Space::Border& gameBorder, const Space::Border& scr
 	}
 
 	spriteManager.LoadAllSprites();
-
-	Enemy::Properties eProp;
-	eProp.hitboxRadius = 4.0f;
-	eProp.spawnFrame = 60;
-	eProp.attackCounter = 120;
-	eProp.numOfAttacks = 10;
-	eProp.health = 20.0f;
-	eProp.rotation = 90;
-	eProp.tempValA = 0.0f;
-	eProp.tempValB = 0.0f;
-	eProp.tempPosA = { 0.0f,0.0f };
-	eProp.tempPosB = { 0.0f,0.0f };
-	eProp.spriteName = SpriteName::DELTA;
-	eProp.color = WHITE;
-
-	enemies.emplace_back(Vector2{ 0.0f,0.0f }, this, eProp);
-	enemies.emplace_back(Vector2{ 5.0f,0.0f }, this, eProp);
-	enemies.emplace_back(Vector2{ 10.0f,0.0f }, this, eProp);
-	enemies.emplace_back(Vector2{ 15.0f,0.0f }, this, eProp);
-
+	LoadEnemies();
 }
 
 LevelScene::~LevelScene()
@@ -60,40 +41,30 @@ LevelScene::~LevelScene()
 
 CurrentScene LevelScene::UpdateScene()
 {
-
-	////// UPDATE //////
 	frameCounter++;
-	player->Update();
-	playerBM.Update();
-	UpdateEnemies();
 
 	//Quit Scene button
 	if (IsKeyPressed(KEY_Q))
 		currentScene = CurrentScene::MENU;
 
-	////// DRAWING //////
-	BeginDrawing();
-	// BACKGROUND //
-	ClearBackground(BLACK);
+	if ((IsLevelCompleted() || player->IsDead()) && IsKeyPressed(KEY_Z))
+	{
+		return CurrentScene::MENU;
+	}
 
-	// PLAYER/ENEMIES //
-	player->DrawSprite();
-	DrawEnemies();
+	////// UPDATE //////
+	if (!IsLevelCompleted() && !player->IsDead())
+	{
+		SpawnEnemies();
+		player->Update();
+		playerBM.Update();
+		enemyBM.Update();
+		UpdateEnemies();
+	}
 
-	// BULLETS //
-	playerBM.DrawBullets();
+	////// DRAW //////
+	DrawScene();
 
-	// PLAYER HITBOX //
-	player->DrawHitbox();
-
-	// UI //
-	DrawUI();
-
-	// DEBUG //
-	DrawHitboxes();
-
-	////// END DRAWING //////
-	EndDrawing();
 	return currentScene;
 }
 
@@ -102,14 +73,23 @@ void LevelScene::PlayerShootTheBullet(const Bullet& bullet)
 	playerBM.InstantiateBullet(bullet);
 }
 
+void LevelScene::EnemyShootTheBullet(const Bullet& bullet)
+{
+	enemyBM.InstantiateBullet(bullet);
+}
+
 void LevelScene::DrawHitboxes()
 {
+	//Bullet Hitbox
 	playerBM.DrawHitboxes({ 0, 121, 241, 63 });
-	DrawCircleV(player->pos, player->GetProperties().hitboxRadius, { 0, 228, 48, 191 }, this);
+	enemyBM.DrawHitboxes({ 230, 41, 55, 127 });
+	//Enemy Hitbox
 	for (const Enemy& enemy : enemies)
 	{
 		DrawCircleV(enemy.pos, enemy.GetProperties().hitboxRadius, { 135, 60, 190, 191 }, this);
 	}
+	//Player Hitbox
+	DrawCircleV(player->pos, player->GetProperties().hitboxRadius, { 0, 228, 48, 191 }, this);
 }
 
 void LevelScene::DrawEnemies()
@@ -120,12 +100,63 @@ void LevelScene::DrawEnemies()
 	}
 }
 
+void LevelScene::DrawScene()
+{
+	////// DEATH SCREEN //////
+	if (player->IsDead())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawSprite(SpriteName::GAME_OVER_TEXT, { 0.0f,-60.0f }, 0, WHITE);
+		//TO DO: draw deathscreen
+		EndDrawing();
+		return;
+	}
+
+	if (IsLevelCompleted())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawSprite(SpriteName::STAGE_TEXT, { 0.0f,-60.0f }, 0, WHITE);
+		DrawSprite(SpriteName::COMPLETED_TEXT, { 0.0f,-46.0f }, 0, WHITE);
+		//TO DO: draw deathscreen
+		EndDrawing();
+		return;
+	}
+
+	////// LEVEL DRAWING //////
+	BeginDrawing();
+
+	// BACKGROUND //
+	ClearBackground(BLACK);
+
+	// PLAYER/ENEMIES //
+	player->DrawSprite();
+	DrawEnemies();
+
+	// BULLETS //
+	playerBM.DrawBullets();
+	enemyBM.DrawBullets();
+
+	// PLAYER HITBOX //
+	player->DrawHitbox();
+
+	// UI //
+	DrawUI();
+
+	// DEBUG //
+	//DrawHitboxes();
+
+	////// END DRAWING //////
+	EndDrawing();
+}
+
 void LevelScene::UpdateEnemies()
 {
 	for (std::vector<Enemy>::iterator enemy = enemies.begin(); enemy != enemies.end();)
 	{
 		//Check Health
-		if (enemy->GetProperties().health <= 0.0f)
+		if (enemy->GetProperties().health <= 0.0f || enemy->GetProperties().lifeTime <= 0)
 		{
 			enemy = enemies.erase(enemy);
 			continue;
@@ -136,7 +167,7 @@ void LevelScene::UpdateEnemies()
 			//TO DO: Replace with player damage
 			enemy->DealDamage(2);
 		}
-
+		enemy->Update();
 		enemy++;
 	}
 }
@@ -162,6 +193,234 @@ void LevelScene::DrawUI()
 
 float LevelScene::AimAtPlayer(const Vector2& pos)
 {
-	return atan2f(pos.y - player->pos.y, pos.x - player->pos.x) * 57.29578f;
+	return atan2f(player->pos.y - pos.y,player->pos.x - pos.x) * 57.29578f;
+}
+
+//Normalised vector directed at player
+Vector2 LevelScene::VectorAtPlayer(const Vector2& pos)
+{
+	float angle = atan2f(player->pos.y - pos.y, player->pos.x - pos.x);
+	return Vector2{ cosf(angle),sinf(angle) };
+}
+
+bool LevelScene::CollidesWithPlayer()
+{
+	return enemyBM.CollidesWithPlayer(*player);
+}
+
+void LevelScene::DestroyAllEnemyBullets()
+{
+	enemyBM.DestroyAllBullets();
+}
+
+bool LevelScene::IsLevelCompleted()
+{
+	if(player->IsDead())
+		return false;
+
+	if (frameCounter >= 60 * 29)
+		return true;
+
+	return false;
+
+}
+
+void LevelScene::SpawnEnemies()
+{
+	for (const Enemy& enemy : enemyList)
+	{
+		if (enemy.GetProperties().spawnFrame == frameCounter)
+		{
+			enemies.push_back(enemy);
+		}
+	}
+}
+
+void LevelScene::LoadEnemies()
+{
+
+	//Sum Enemy
+	Enemy::Properties eProp1;
+	eProp1.lifeTime = 360;
+	eProp1.hitboxRadius = 4.0f;
+	eProp1.spawnFrame = 60;
+	eProp1.attackCounter = 0;
+	eProp1.numOfAttacks = 10;
+	eProp1.health = 140.0f;
+	eProp1.rotation = 90;
+	eProp1.tempValA = 0.0f;
+	eProp1.tempValB = 0.0f;
+	eProp1.tempPosA = { 0.0f,0.0f };
+	eProp1.tempPosB = { 0.0f,0.0f };
+	eProp1.spriteName = SpriteName::BIG_SUM;
+	eProp1.color = WHITE;
+	eProp1.ShootFunction = [](Enemy* enemy)
+		{
+			Bullet baseBullet;
+			baseBullet.isActive = true;
+			baseBullet.canDamagePlayer = true;
+			baseBullet.lifetime = 60 * 8;
+			baseBullet.pos = enemy->pos;
+			baseBullet.velocity = enemy->VectorAtPlayer();
+			baseBullet.acceleration = { 0.0f,0.0f };
+			baseBullet.rotation = enemy->AimAtPlayer();
+			baseBullet.hitboxRadius = 1.0f;
+			baseBullet.spriteName = SpriteName::INCLUSION;
+			baseBullet.color = WHITE;
+			baseBullet.UpdateFunction = [](Bullet* bullet) { bullet->PhysicsUpdate(); };
+
+			if (enemy->CanAttack(2))
+			{
+				enemy->properties.tempValA += 0.2;
+				if(enemy->properties.tempValA >= 3)
+					enemy->properties.tempValA = 0;
+				Bullet instanceBullet = baseBullet;
+				instanceBullet.velocity = instanceBullet.velocity + instanceBullet.velocity * enemy->properties.tempValA;
+
+				enemy->InstantiateBullet(instanceBullet);
+			}
+		};
+	eProp1.UpdateFunction = [](Enemy* enemy) { enemy->LookAtPlayer(0); enemy->Move(Vector2{ 0.0f,0.0f }); enemy->Shoot(); };
+
+
+	//Delta Enemy
+	Enemy::Properties eProp;
+	eProp.lifeTime = 130;
+	eProp.hitboxRadius = 4.0f;
+	eProp.spawnFrame = 60;
+	eProp.attackCounter = 0;
+	eProp.numOfAttacks = 10;
+	eProp.health = 10.0f;
+	eProp.rotation = 90;
+	eProp.tempValA = 0.0f;
+	eProp.tempValB = 0.0f;
+	eProp.tempPosA = { 0.0f,0.0f };
+	eProp.tempPosB = { 0.0f,0.0f };
+	eProp.spriteName = SpriteName::DELTA;
+	eProp.color = WHITE;
+	eProp.ShootFunction = [](Enemy* enemy)
+		{
+			Bullet baseBullet;
+			baseBullet.isActive = true;
+			baseBullet.canDamagePlayer = true;
+			baseBullet.lifetime = 60*4;
+			baseBullet.pos = enemy->pos;
+			baseBullet.velocity = enemy->VectorAtPlayer();
+			baseBullet.acceleration = { 0.0f,0.0f };
+			baseBullet.rotation = enemy->AimAtPlayer();
+			baseBullet.hitboxRadius = 1.0f;
+			baseBullet.spriteName = SpriteName::HERMIT;
+			baseBullet.color = WHITE;
+			baseBullet.UpdateFunction = [](Bullet* bullet) { bullet->PhysicsUpdate(); };
+
+			if (enemy->CanAttack(30))
+				enemy->InstantiateBulletCircleWall(baseBullet,10);
+		};
+	eProp.UpdateFunction = [](Enemy* enemy) { enemy->LookAtPlayer(0); enemy->Move(Vector2{ 1.0f,0.0f }); enemy->Shoot(); };
+
+
+	enemyList.emplace_back(Vector2{ -60.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ -65.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ -70.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ -75.0f,-10.0f }, this, eProp);
+
+	eProp.spawnFrame = 60 * 3;
+	eProp.UpdateFunction = [](Enemy* enemy) { enemy->LookAtPlayer(0); enemy->Move(Vector2{ -1.0f,0.0f }); enemy->Shoot(); };
+
+	enemyList.emplace_back(Vector2{ 60.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ 65.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ 70.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ 75.0f,-10.0f }, this, eProp);
+
+	eProp.spawnFrame = 60 * 5;
+
+	enemyList.emplace_back(Vector2{ 60.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ 65.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ 70.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ 75.0f,-10.0f }, this, eProp);
+
+	eProp.UpdateFunction = [](Enemy* enemy) { enemy->LookAtPlayer(0); enemy->Move(Vector2{ 1.0f,0.0f }); enemy->Shoot(); };
+
+	enemyList.emplace_back(Vector2{ -60.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ -65.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ -70.0f,-10.0f }, this, eProp);
+	enemyList.emplace_back(Vector2{ -75.0f,-10.0f }, this, eProp);
+
+
+	eProp1.spawnFrame = 60 * 7;
+	eProp1.UpdateFunction = [](Enemy* enemy) { enemy->LookAtPlayer(0); enemy->Move(Vector2{ 0.0f,0.2f }); enemy->Shoot(); };
+	enemyList.emplace_back(Vector2{   0.0f,-80.0f }, this, eProp1);
+
+	eProp1.spawnFrame = 60 * 10;
+	enemyList.emplace_back(Vector2{ 40.0f,-80.0f }, this, eProp1);
+	enemyList.emplace_back(Vector2{ -40.0f,-80.0f }, this, eProp1);
+	
+
+	//For All Enemy
+	Enemy::Properties eProp2;
+	eProp2.lifeTime = 130;
+	eProp2.hitboxRadius = 4.0f;
+	eProp2.spawnFrame = 60;
+	eProp2.attackCounter = 0;
+	eProp2.numOfAttacks = 10;
+	eProp2.health = 50.0f;
+	eProp2.rotation = 90;
+	eProp2.tempValA = 0.0f;
+	eProp2.tempValB = 0.0f;
+	eProp2.tempPosA = { 0.0f,0.0f };
+	eProp2.tempPosB = { 0.0f,0.0f };
+	eProp2.spriteName = SpriteName::FOR_ALL;
+	eProp2.color = WHITE;
+	eProp2.ShootFunction = [](Enemy* enemy)
+		{
+			Bullet baseBullet;
+			baseBullet.isActive = true;
+			baseBullet.canDamagePlayer = true;
+			baseBullet.lifetime = 60 * 4;
+			baseBullet.pos = enemy->pos;
+			baseBullet.velocity = enemy->VectorAtPlayer();
+			baseBullet.acceleration = { 0.0f,0.0f };
+			baseBullet.rotation = enemy->AimAtPlayer();
+			baseBullet.hitboxRadius = 1.0f;
+			baseBullet.spriteName = SpriteName::AND;
+			baseBullet.color = WHITE;
+			baseBullet.UpdateFunction = [](Bullet* bullet) { bullet->PhysicsUpdate(); };
+
+			if (enemy->CanAttack(2))
+				enemy->InstantiateBulletCircle(baseBullet, 50);
+		};
+	eProp2.UpdateFunction = [](Enemy* enemy) { enemy->Move(Vector2{ 0.0f,0.0f }); enemy->Shoot(); };
+
+	eProp2.spawnFrame = 60 *16;
+	enemyList.emplace_back(Vector2{   0.0f,-60.0f }, this, eProp2);
+	eProp2.spawnFrame = 60 *19;
+	enemyList.emplace_back(Vector2{  40.0f,-60.0f }, this, eProp2);
+	enemyList.emplace_back(Vector2{ -40.0f,-60.0f }, this, eProp2);
+	eProp2.spawnFrame = 60 * 24;
+	eProp2.lifeTime = 100;
+	eProp2.ShootFunction = [](Enemy* enemy)
+		{
+			Bullet baseBullet;
+			baseBullet.isActive = true;
+			baseBullet.canDamagePlayer = true;
+			baseBullet.lifetime = 60 * 4;
+			baseBullet.pos = enemy->pos;
+			baseBullet.velocity = enemy->VectorAtPlayer();
+			baseBullet.acceleration = { 0.0f,0.0f };
+			baseBullet.rotation = enemy->AimAtPlayer();
+			baseBullet.hitboxRadius = 1.0f;
+			baseBullet.spriteName = SpriteName::AND;
+			baseBullet.color = WHITE;
+			baseBullet.UpdateFunction = [](Bullet* bullet) { bullet->PhysicsUpdate(); };
+
+			if (enemy->CanAttack(2))
+				enemy->InstantiateBulletCircle(baseBullet, 30);
+		};
+
+	enemyList.emplace_back(Vector2{ 40.0f,-60.0f }, this, eProp2);
+	enemyList.emplace_back(Vector2{ -40.0f,-60.0f }, this, eProp2);
+	enemyList.emplace_back(Vector2{ -40.0f, 60.0f }, this, eProp2);
+	enemyList.emplace_back(Vector2{  40.0f, 60.0f }, this, eProp2);
+
 }
 
--- src/scenes/baseLevel.hpp
+++ src/scenes/baseLevel.hpp
@@ -21,14 +21,24 @@ public:
 	~LevelScene();
 	CurrentScene UpdateScene();
 	void PlayerShootTheBullet(const Bullet& bullet);
+	void EnemyShootTheBullet(const Bullet& bullet);
 	void DrawHitboxes();
 	void DrawEnemies();
+	void DrawScene();
 	void UpdateEnemies();
 	void DrawUI();
 	float AimAtPlayer(const Vector2& pos);
+	Vector2 VectorAtPlayer(const Vector2& pos);
+	//Check if bullets/enemies collide
+	bool CollidesWithPlayer();
+	void DestroyAllEnemyBullets();
+	bool IsLevelCompleted();
+	void SpawnEnemies();
+	void LoadEnemies();
 private:
 	std::unique_ptr<Player> player;
-	std::vector<Enemy> enemies;
+	std::vector<Enemy> enemies; //Enemies on screen
+	std::vector<Enemy> enemyList; //list of all enemies on level that will be spawned
 	BulletManager enemyBM;
 	BulletManager playerBM;
 	CurrentScene currentScene;
